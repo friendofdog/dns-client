b160fd9849e389e550970d271d99383b0e43712b
Kevin Kee <kevinkee9@gmail.com>
2020-04-21 02:42:44 +0000

    Get domain, record type, and server address from command line args

--- dns_client.py
+++ dns_client.py
@@ -1,15 +1,28 @@
 import dns.resolver
+import argparse
 
-def get_records(domain, record, *ns):
+def get_records(args):
+    print(args)
+    domain, record, server = args.values()
+    server = (server,)
     resolver = dns.resolver.Resolver(configure=False)
-    resolver.nameservers = ns
-
+    resolver.nameservers = server
+    print(domain, record)
     answer = resolver.query(domain, record)
-
     return answer
 
+def get_query_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--domain', help="domain to query")
+    parser.add_argument('-r', '--record', help="dns record type")
+    parser.add_argument('-s', '--server', help="server ip address")
+    args = parser.parse_args()
+    args_dict = vars(args)
+    return args_dict
+
 if __name__ == "__main__":
-    records = get_records('yahoo.com', 'A', '8.8.8.8')
+    query_args = get_query_args()
+    records = get_records(query_args)
     for record in records:
         print(record)
 
--- test_dns_client.py
+++ test_dns_client.py
@@ -4,9 +4,18 @@ from dns_client import get_records
 import dns.resolver
 import mock
 from mock import patch
-
+import re
 
 def test_get_records():
     with patch('dns.resolver.query', mock) as dns.resolver.query:
-        ip = get_records('google.com', 'A', '8.8.8.8')[0]
+        ip = get_records(
+            {'domain': 'google.com', 'record': 'A', 'server': '8.8.8.8'}
+        )[0]
+        # class is the type which dns.resolver is expected to return
+        assert isinstance(ip, dns.rdtypes.IN.A.A)
+        # ipv4 address can be split into four parts
         assert len(str(ip).split('.')) == 4
+        # parts of address match parts of ipv4 address
+        match = re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", str(ip))
+        assert bool(match)
+
